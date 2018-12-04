9bbdfe6647a0ec891659edfbb21a55448ff607a5
Henry Longmore <henry@longmore.org>
2018-12-04 11:00:54 -0700

    Update due to refactor

--- tests/test_geodistance.py
+++ tests/test_geodistance.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-
+import coordinates
 import geodistance
 
 
@@ -11,15 +11,15 @@ class TestGeoDistance(TestCase):
         self.new_york = (41.501299, -074.620909)
 
     def test_new_york_to_rhode_island_metric(self):
-        ri = geodistance.LatLongCoordinate(*self.rhode_island)
-        ny = geodistance.LatLongCoordinate(*self.new_york)
+        ri = coordinates.LatLongCoordinate(*self.rhode_island)
+        ny = coordinates.LatLongCoordinate(*self.new_york)
         gd = geodistance.GeoDistance(ri, ny)
-        distance = gd.keerthana()
+        distance = gd.oblate_shperoid()
         self.assertAlmostEqual(distance, 264.842, places=3)
 
     def test_new_york_to_rhode_island_imperial(self):
-        ri = geodistance.LatLongCoordinate(*self.rhode_island)
-        ny = geodistance.LatLongCoordinate(*self.new_york)
+        ri = coordinates.LatLongCoordinate(*self.rhode_island)
+        ny = coordinates.LatLongCoordinate(*self.new_york)
         gd = geodistance.GeoDistance(ri, ny)
-        distance = gd.keerthana(units='imperial')
+        distance = gd.oblate_shperoid(units='imperial')
         self.assertAlmostEqual(distance, 164.565, places=3)
